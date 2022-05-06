60889cfac05776b2cad47a4a6eb034559237a9b4
undefined <undefinedstepan@gmail.com>
2022-05-06 15:30:42 +0200

    Initial commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use http\Env\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
